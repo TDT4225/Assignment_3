c6f3abfbf50c4eecec7544c342c830d451af73cf
TrygveJW <git@woldseth.xyz>
2021-10-22 11:09:56 +0200

    enble all

--- src/main/java/no/ntnu/stodist/main.java
+++ src/main/java/no/ntnu/stodist/main.java
@@ -15,18 +15,18 @@ public class main {
 
             MongoDatabase db = db_connection.getMongoConnection();
             //Assignment3Tasks.insertData(db);
-            //Assignment3Tasks.task1(db);
-            //Assignment3Tasks.task2(db);
-            //Assignment3Tasks.task3(db);
-            //Assignment3Tasks.task4(db);
-            //Assignment3Tasks.task5(db);
-            //Assignment3Tasks.task6(db);
-            //Assignment3Tasks.task7(db);
-            //Assignment3Tasks.task8(db);
+            Assignment3Tasks.task1(db);
+            Assignment3Tasks.task2(db);
+            Assignment3Tasks.task3(db);
+            Assignment3Tasks.task4(db);
+            Assignment3Tasks.task5(db);
+            Assignment3Tasks.task6(db);
+            Assignment3Tasks.task7(db);
+            Assignment3Tasks.task8(db);
             Assignment3Tasks.task9(db);
-            //Assignment3Tasks.task10(db);
-            //Assignment3Tasks.task11(db);
-            //Assignment3Tasks.task12(db);
+            Assignment3Tasks.task10(db);
+            Assignment3Tasks.task11(db);
+            Assignment3Tasks.task12(db);
 
 
         } catch (Exception e) {
